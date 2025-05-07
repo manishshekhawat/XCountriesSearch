b7166b7f339e9fe2372de3cac15059960dbfd04c
Manish <manishshekhawat756@gmail.com>
2025-05-07 22:25:50 +0530

    countries

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import Countries from "./countires";
+
+function App() {
+  //const number=[1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch("https://xcountries-backend.azurewebsites.net/all")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setItem(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "10px",
+          
+        }}
+      >
+        {item.map((data) => {
+          return <Countries name={data.name} flag={data.flag} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Countires.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Countries = ({ name, flag }) => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap:"5px",
+          height: "200px",
+          width: "200px",   
+          border: "1px solid grey",
+          borderRadius: "10px",
+          textAlign:"center"
+        }}
+      >
+        <img
+          src={flag}
+          alt={name}
+          style={{ width:"80px",height:"80px" }}
+        />
+        <h2>{name}</h2>
+      </div>
+    </>
+  );
+};
+
+export default Countries;
